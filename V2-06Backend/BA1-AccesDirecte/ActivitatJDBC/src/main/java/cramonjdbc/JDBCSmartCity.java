41a51897b11d4887b16391e8012494f65584c4fe
Carles Ramon Ruiz <42216936+CarlesRamonRuiz@users.noreply.github.com>
2026-09-19 13:40:24 +0200

    SuperJDBC

--- V2-06Backend/BA1-AccesDirecte/ActivitatJDBC/src/main/java/cramonjdbc/JDBCSmartCity.java
+++ V2-06Backend/BA1-AccesDirecte/ActivitatJDBC/src/main/java/cramonjdbc/JDBCSmartCity.java
@@ -8,7 +8,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
+
 
 public class JDBCSmartCity {
 	static String url = "jdbc:mysql://localhost:3306/ciutat_sensors";
@@ -53,6 +55,7 @@ public class JDBCSmartCity {
 
 	}
 
+	// 2
 	public static int registraLectura(String nomSensor, double valor) {
 		try (Connection connection = DriverManager.getConnection(url, user, password)) {
 
@@ -170,8 +173,7 @@ public class JDBCSmartCity {
 			PreparedStatement prepStatement = connection.prepareStatement(query);
 
 			String dataS = data.toString();
-			
-			
+
 			prepStatement.setString(1, dataS);
 
 			int linies = prepStatement.executeUpdate();
@@ -187,22 +189,63 @@ public class JDBCSmartCity {
 	}
 
 	public static void netejaLecturesError(LocalDate data) {
-		try(Connection connection = DriverManager.getConnection(url, user, password)) {
-			
-			String query = "DELETE FROM lectures\n"
-					+ "WHERE qualitat = 'ERROR' AND data_lectura < ? ;";
-			
+		try (Connection connection = DriverManager.getConnection(url, user, password)) {
+
+			String query = "DELETE FROM lectures\n" + "WHERE qualitat = 'ERROR' AND data_lectura < ? ;";
+
 			PreparedStatement prepStatement = connection.prepareStatement(query);
-			
+
 			String dataS = data.toString();
-			
-			
+
 			prepStatement.setString(1, dataS);
-			
+
 			int linies = prepStatement.executeUpdate();
 
 			System.out.println(linies);
 
+			prepStatement.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void eliminaZonesBuides() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)) {
+			String query = "DELETE FROM zones \r\n" + "WHERE id NOT IN (SELECT zona_id  FROM sensors);";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			int linies = prepStatement.executeUpdate();
+
+			System.out.println(linies);
+
+			prepStatement.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 3
+	public static void sensorsSenseLectures() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)) {
+			String query = "SELECT s.nom nomSensor, ts.nom nomTipusSensor, z.nom nomZona\r\n" + "FROM sensors s \r\n"
+					+ "JOIN tipus_sensors ts ON s.tipus_sensor_id = ts.id\r\n" + "JOIN zones z ON s.zona_id = z.id\r\n"
+					+ "WHERE s.id NOT IN (SELECT l.sensor_id  FROM lectures l) \r\n" + "ORDER BY z.id ,s.nom \r\n"
+					+ ";";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			ResultSet resultSet = prepStatement.executeQuery();
+			while (resultSet.next()) {
+				String nomS = resultSet.getString(1);
+				String nomTS = resultSet.getString(2);
+				String nomZ = resultSet.getString(3);
+
+				System.out.println("· Nom sensor: " + nomS + " Nom tipus de sensor: " + nomTS + " Nom de zona: " + nomZ);
+			}
+			resultSet.close();
 			prepStatement.close();
 			
 		} catch (SQLException e) {
@@ -210,11 +253,262 @@ public class JDBCSmartCity {
 		}
 	}
 
-	public static void eliminaZonesBuides() {
-		
+	public static void lecturesDeZona(String nomZona, String qualitat) {
+		try (Connection connection = DriverManager.getConnection(url, user, password)) {
+			String query = "SELECT s.nom, ts.nom , l.valor , ts.unitat_mesura , l.data_lectura \r\n"
+					+ "FROM sensors s \r\n"
+					+ "JOIN tipus_sensors ts ON s.tipus_sensor_id = ts.id\r\n"
+					+ "JOIN zones z  ON s.zona_id = z.id\r\n"
+					+ "JOIN lectures l ON s.id = l.sensor_id\r\n"
+					+ "WHERE z.nom = ? AND l.qualitat =?\r\n"
+					+ "ORDER BY l.data_lectura desc\r\n"
+					+ ";";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+			prepStatement.setString(1, nomZona);
+			prepStatement.setString(2, qualitat);
+			ResultSet resultSet = prepStatement.executeQuery();
+			while (resultSet.next()) {
+				String nomS = resultSet.getString(1);
+				String nomTS = resultSet.getString(2);
+				double valor = resultSet.getDouble(3);
+				String mesura = resultSet.getString(4);
+				Date data = resultSet.getDate(5);
+				
+
+				System.out
+						.println("· Nom sensor: " + nomS + " Nom tipus de sensor: " + nomTS + " Valor: "+valor+" Unitat de mesura: "+mesura+ " Data:"+data );
+			}
+			resultSet.close();
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	// 4
+	public static void resumZones() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "SELECT z.nom , count(s.id ) nombreSensors, COUNT(s.actiu) nombreActius\r\n"
+					+ "FROM zones z\r\n"
+					+ "JOIN sensors s ON z.id =s.zona_id \r\n"
+					+ "WHERE s.actiu = 1 AND z.id = s.zona_id \r\n"
+					+ "GROUP BY z.nom\r\n"
+					+ "ORDER BY nombresensors desc;";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			ResultSet resultSet = prepStatement.executeQuery();
+			while (resultSet.next()) {
+				String nomZ = resultSet.getString(1);
+				int nombreSensors = resultSet.getInt(2);
+				int nombreActius = resultSet.getInt(3);
+
+				System.out.println("· Nom zona: " + nomZ + " Nombre de sensors: " + nombreSensors + " Nombre de sensors actius: " + nombreActius);
+			}
+			resultSet.close();
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void estadistiquesPerTipus() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "SELECT ts.nom , MIN(l.valor ) valorMinim, MAX(l.valor ) valorMaxim, AVG(l.valor) valorAVG\r\n"
+					+ "FROM tipus_sensors ts\r\n"
+					+ "JOIN sensors s  ON ts.id   = s.tipus_sensor_id  \r\n"
+					+ "JOIN lectures l ON s.id = l.sensor_id\r\n"
+					+ "WHERE l.qualitat = 'OK'\r\n"
+					+ "GROUP BY ts.nom\r\n"
+					+ "HAVING COUNT(l.qualitat ='OK')>1\r\n"
+					+ "ORDER BY valoravg desc\r\n"
+					+ ";";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			ResultSet resultSet = prepStatement.executeQuery();
+			while (resultSet.next()) {
+				String nomTS = resultSet.getString(1);
+				int valorMinim = resultSet.getInt(2);
+				int valorMaxim = resultSet.getInt(3);
+				int valorAVG = resultSet.getInt(4);
+
+				System.out.println("· Nom Tipus sensor: " + nomTS + ", Valor Minim: " + valorMinim + ", Valor Màxim: " + valorMaxim+ ", Valor Mitjà: "+ valorAVG);
+			}
+			resultSet.close();
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	// 5
+	public static void crearNovaTaula() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "CREATE TABLE IF NOT EXISTS manteniment (\r\n"
+					+ "	\r\n"
+					+ "	id int AUTO_INCREMENT PRIMARY KEY ,\r\n"
+					+ "	sensor_id int NOT NULL,\r\n"
+					+ "	data_intervencio date NOT null,\r\n"
+					+ "	tecnic varchar(100) NOT NULL,\r\n"
+					+ "	tipus enum('PREVENTIU','CORRECTIU') NOT NULL ,\r\n"
+					+ "	cost Decimal(8,2) NOT NULL ,\r\n"
+					+ "	descripcio varchar(255),\r\n"
+					+ "	FOREIGN KEY (sensor_id) REFERENCES sensors(id)\r\n"
+					+ "\r\n"
+					+ ");";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+			
+			System.out.println("Taula creada.");
+
+			
+			prepStatement.close();
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+	}
+	public static void inserirManteniment(int id_sensor, LocalDate data,String tecnic, String tipus, int cost, String desc) {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "INSERT INTO ciutat_sensors.manteniment\r\n"
+					+ "(sensor_id, data_intervencio, tecnic, tipus, cost, descripcio)\r\n"
+					+ "VALUES(?, ?, ?, ?, ?, ?);";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+			
+			String dataS = data.toString() ;
+			
+			prepStatement.setInt(1, id_sensor);
+			prepStatement.setString(2, dataS);
+			prepStatement.setString(3, tecnic);
+			prepStatement.setString(4, tipus);
+			prepStatement.setInt(5, cost);
+			prepStatement.setString(6, desc);
+			
+			int linies = prepStatement.executeUpdate();
+			
+			System.out.println("Linies inserides: "+linies);
+			
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	//6
+	public static void ultim_manteniment () {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "ALTER TABLE sensors\r\n"
+					+ "ADD column ultim_manteniment Date ;";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			
+			prepStatement.executeUpdate();
+			
+			System.out.println("Columna creada");
+			
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void afegir_unique() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "ALTER TABLE sensors\r\n"
+					+ "MODIFY nom varchar(100) UNIQUE;";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			
+			prepStatement.executeUpdate();
+			
+			System.out.println("Columna modificada");
+			
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	
-	
-}
+	public static void afegirDuplicat() {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "INSERT INTO ciutat_sensors.sensors\r\n"
+					+ "(nom, zona_id, tipus_sensor_id, pos_x, pos_y, altura, orientacio, actiu, data_instalacio, ultim_manteniment)\r\n"
+					+ "VALUES('TEMP-CENTRE-01', 1, 1, 100, 200, 3.00, 1, 1, CURRENT_DATE(), NULL);";
 
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+
+			
+			prepStatement.executeUpdate();
+			
+			System.out.println("Columna modificada");
+			
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//7
+	public static void cercaSensors(String text, Integer idTipus, Boolean nomesActius) {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "SELECT nom  FROM sensors\r\n"
+					+ "WHERE (? IS NULL OR actiu = ?) \r\n"
+					+ "AND (? IS NULL OR nom LIKE ?) \r\n"
+					+ "AND (? IS NULL OR tipus_sensor_id =?);";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+			
+			prepStatement.setBoolean(1, nomesActius);
+			prepStatement.setBoolean(2, nomesActius);
+			prepStatement.setString(3, text);
+			prepStatement.setString(4, "%"+text+"%");
+			prepStatement.setInt(5, idTipus);
+			prepStatement.setInt(6, idTipus);
+			
+			ResultSet resultSet = prepStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				String nomS = resultSet.getString(1);
+				System.out.println("Nom de Sensor: "+nomS);
+			}
+			
+			
+			
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void lecturesEntreDates(int idSensor, LocalDateTime inici, LocalDateTime fi) {
+		try (Connection connection = DriverManager.getConnection(url, user, password)){
+			String query = "SELECT s.nom , sum((CASE WHEN l.qualitat = 'OK' THEN 1 ELSE 0 END)) qOk, Sum((CASE WHEN l.qualitat = 'ERROR' THEN 1 ELSE 0 END)) qError, Sum((CASE WHEN l.qualitat = 'DUBTOSA' THEN 1 ELSE 0 END)) qDubtosa  FROM  lectures l\r\n"
+					+ "JOIN sensors s ON l.sensor_id = s.id\r\n"
+					+ "WHERE l.data_lectura BETWEEN ? AND ? AND l.sensor_id = ? \r\n"
+					+ "GROUP BY s.nom \r\n"
+					+ ";";
+
+			PreparedStatement prepStatement = connection.prepareStatement(query);
+			String iniciS = inici.toString();
+			String fiS = fi.toString();
+			prepStatement.setString(1, iniciS);
+			prepStatement.setString(2, fiS);
+			prepStatement.setInt(3, idSensor);
+			
+
+			
+			ResultSet resultSet = prepStatement.executeQuery();
+			
+			while (resultSet.next()) {
+				String nomS = resultSet.getString(1);
+				System.out.println("Nom de Sensor: "+nomS);
+			}
+			
+			
+			resultSet.close();
+			prepStatement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	//8
+
+}
--- V2-06Backend/BA1-AccesDirecte/ActivitatJDBC/src/main/java/cramonjdbc/main.java
+++ V2-06Backend/BA1-AccesDirecte/ActivitatJDBC/src/main/java/cramonjdbc/main.java
@@ -1,6 +1,8 @@
 package cramonjdbc;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class main {
@@ -26,16 +28,48 @@ static Scanner sc = new Scanner(System.in);
 		//JDBCSmartCity.desactivaSensorsSilenciosos(LocalDate.of(sc.nextInt(), sc.nextInt(), sc.nextInt()));
 		System.out.println("DELETES");
 		System.out.println("Neteja d'errors");
-		JDBCSmartCity.netejaLecturesError(LocalDate.of(sc.nextInt(), sc.nextInt(), sc.nextInt()));
-		
+		//JDBCSmartCity.netejaLecturesError(LocalDate.of(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+		System.out.println("Eliminar zones buides");
+		//JDBCSmartCity.eliminaZonesBuides();
 		System.out.println("____________________________________");
-		// 2. OPERACIONS CRUD
-		System.out.println("2. Operacions CRUD");
+		// 3. CONSULTES AMB JOINS
+		System.out.println("3. CONSULTES AMB JOINS");
+		System.out.println("Sensors sense lectures");
+		//JDBCSmartCity.sensorsSenseLectures();
+		System.out.println("Lecutures de zona, posa nom de la zona i qualitat");
+		//JDBCSmartCity.lecturesDeZona(sc.nextLine(), sc.nextLine());
+		System.out.println("____________________________________");
+		System.out.println("4. AGREGACIONS AMB GROUP BY");
+		System.out.println("Resum de zones");
+		//JDBCSmartCity.resumZones();
+		System.out.println("Estadistiques per tipus");
+		//JDBCSmartCity.estadistiquesPerTipus();
+		System.out.println("____________________________________");
+		System.out.println("5. CREACIÓ DE NOVA TAULA");
+		//JDBCSmartCity.crearNovaTaula();
+		System.out.println("Insercions");
+		//JDBCSmartCity.inserirManteniment(2, LocalDate.of(2026, 9, 27), "Mateo", "CORRECTIU", 110, null);
+		//JDBCSmartCity.inserirManteniment(2, LocalDate.of(2026, 10, 22), "Ivan", "PREVENTIU", 0, null);
+		//JDBCSmartCity.inserirManteniment(2, LocalDate.of(2026, 1, 1), "Biel", "CORRECTIU", 210, null);
+		System.out.println("____________________________________");
+		System.out.println("6. ALTER TABLE");
+		System.out.println("Crear columna Ultim manteniment");
+		//JDBCSmartCity.ultim_manteniment();
+		//JDBCSmartCity.afegir_unique();
+		//JDBCSmartCity.afegirDuplicat();
+		System.out.println("____________________________________");
+		System.out.println("7. PREPARED STATEMENTS");
+		JDBCSmartCity.cercaSensors("E", 1, true);
 		System.out.println();
+		JDBCSmartCity.cercaSensors("C", 4, false);
+		System.out.println();
+		JDBCSmartCity.cercaSensors("U", 2, true);
+		
+		System.out.println("Consulta per rang de dates");
+		JDBCSmartCity.lecturesEntreDates(1, LocalDateTime.of(LocalDate.of(2026,9, 1), LocalTime.of(7, 10)), LocalDateTime.of(LocalDate.of(2026, 10, 9), LocalTime.of(1, 10)));
 		System.out.println("____________________________________");
-		System.out.println("____________________________________");
-		System.out.println("____________________________________");
-
+		System.out.println("8. BATCH PROCESSING");
+		
 	}
 
 }
